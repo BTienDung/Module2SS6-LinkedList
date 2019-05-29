119c1ae589c492cf4612687944fcfbf72d6bcf8a
BTienDung <tiendung0899@gmail.com>
2019-05-29 09:06:09 +0700

    nb

---
+++ TestMyLinkedList.java
@@ -0,0 +1,12 @@
+package season_6.LinkedList;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        MyLinkedList li = new MyLinkedList();
+        li.addFirst(11);
+        li.addFirst(22);
+
+        li.printList();
+    }
+}
